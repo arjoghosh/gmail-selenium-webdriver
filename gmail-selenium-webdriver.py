1cd986dd3c8720128b3c5712e683ca3442e8840d
Arjo Ghosh আর্য্য ঘোষ <arjoghosh@outlook.com>
2016-05-26 11:04:21 +0530

    Create gmail-selenium-webdriver.py

---
+++ gmail-selenium-webdriver.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+x=input("Enter username :\t")
+x=x.strip()
+y=input("Enter password :\t")
+browser = webdriver.Firefox()
+browser.get('http://gmail.com')
+action = webdriver.ActionChains(browser)
+emailElem = browser.find_element_by_id('Email')
+emailElem.send_keys(x)
+nextButton = browser.find_element_by_id('next')
+nextButton.click()
+time.sleep(1)
+passwordElem = browser.find_element_by_id('Passwd')
+passwordElem.send_keys(y)
+signinButton = browser.find_element_by_id('signIn')
+signinButton.click()
